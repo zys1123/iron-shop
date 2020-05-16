4eb1ab21593b9c6a1f685ed2263928247c5c5e61
zys1123 <987986567@qq.com>
2020-05-16 09:21:39 +0800

    --create 新建项目

---
+++ src/main/java/com/zys/ironshop/IronshopApplication.java
@@ -0,0 +1,13 @@
+package com.zys.ironshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronshopApplication.class, args);
+	}
+
+}
