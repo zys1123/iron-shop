4760c39d65bd29fb7cbefb5a1cd489ec30f47ee8
zys1123 <987986567@qq.com>
2020-05-16 09:21:39 +0800

    --create 新建项目

---
+++ src/test/java/com/zys/ironshop/IronshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zys.ironshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
